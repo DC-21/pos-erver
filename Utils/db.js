e43a2535115d0394337929211c89ad4b9c314111
DC-21 <cholahkuboko@gmail.com>
2023-07-30 13:37:36 +0200

    first commit

---
+++ Utils/db.js
@@ -0,0 +1,9 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('businessDB', 'Cholah', 'Cholah@2104', {
+  host: 'localhost',
+  port: 3306,
+  dialect: 'mysql',
+  logging: true,
+});
+module.exports=sequelize;
---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const sequelize = require('./Utils/db.js');
+const bodyParser=require('body-parser');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended:true}));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}));
+app.use('/',require('./routes/router'))
+
+app.use((err, red, res, next)=>{
+    console.log(err);
+    const status = err.statusCode || 500;
+    const message = err.message;
+    res.status(status).json({ message: message });
+});
+
+app.get('/',(req,res)=>{
+    res.send("The api is working");
+})
+
+sequelize.sync().then(()=>{
+    console.log("Database successfully connected");
+    app.listen(3006)
+    console.log("App listening on http://localhost:3006");
+}).catch(err=>{
+    console.log(err);
+});

e113656b166531252de6792f64711bbde5e29917
DC-21 <cholahkuboko@gmail.com>
2023-08-06 21:32:07 +0200

    first commit

--- models/User.js
+++ models/User.js
@@ -2,36 +2,38 @@ const Sequelize = require('sequelize');
 const db = require('../Utils/db');
 
 const User = db.define('User', {
-    id:{
-        type:Sequelize.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-        allowNull:false,
-    },
-    full_name:{
-        type:Sequelize.STRING,
-        allowNull: false,
-        required:true,
-    },
-    email:{
-        type:Sequelize.STRING,
-        allowNull: false,
-        required:true,
-        unique:true,
-    },
-    phone_number:{
-        type:Sequelize.INTEGER,
-        allowNull: false,
-        required:true,
-    },
-    password:{
-        type:Sequelize.STRING,
-        allowNull: false,
-    },
-    role: {
-        type: Sequelize.STRING,
-        allowNull: false
-    }
-})
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  full_name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    required: true,
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    required: true,
+    unique: true,
+  },
+  phone_number: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    required: true,
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+}, {
+  timestamps: false,
+});
 
 module.exports = User;
